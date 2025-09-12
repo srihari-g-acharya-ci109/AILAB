60c77e6b5981f66d46853c5aa4fabb449ab85657
sriharigacharya <sriharigacharya@gmail.com>
2025-09-12 09:46:14 +0530

    Lab 1

---
+++ 5th Sem/And Gate.py
@@ -0,0 +1,13 @@
+w1=int(input("Enter Weight 1:"))
+w2=int(input("Enter Weight 2:"))
+t=int(input("Enter threshold:"))
+
+values=[0,1]
+
+print("x1\t x2\ty")
+for i in values:
+    for j in values:
+        temp = 1 if ((w1 * i) + (w2 * (j))) >= t else 0
+        print(i,"\t",j,"\t",temp)
+
+
