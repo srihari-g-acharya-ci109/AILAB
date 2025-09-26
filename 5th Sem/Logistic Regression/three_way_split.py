6c003630d2b97cfd904afa8aad74348832d0525f
sriharigacharya <sriharigacharya@gmail.com>
2025-09-26 10:24:12 +0530

    Confusion matrix added

--- 5th Sem/Logistic Regression/three_way_split.py
+++ 5th Sem/Logistic Regression/three_way_split.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 df = pd.read_csv(r"C:\Users\sriha\OneDrive\Documents\GitHub\College\AILAB\5th Sem\Logistic Regression\dummy_dataset_5200.csv")
 
@@ -30,6 +30,11 @@ test_acc = accuracy_score(y_test, model.predict(X_test))
 print("Train size:", X_train.shape, "Validation size:", X_val.shape, "Test size:", X_test.shape)
 print("Validation Accuracy:", val_acc)
 print("Test Accuracy:", test_acc)
+
+
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print(cm)
 
 
 sample_data = pd.DataFrame([[35, 1, 50000]], columns=["Age", "Gender", "AccountBalance"])
--- 5th Sem/Logistic Regression/two_way_splitt.py
+++ 5th Sem/Logistic Regression/two_way_splitt.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 df = pd.read_csv(r"C:\Users\sriha\OneDrive\Documents\GitHub\College\AILAB\5th Sem\Logistic Regression\dummy_dataset_5200.csv")
 
@@ -21,6 +21,10 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 print("2-Way Split Accuracy:", accuracy_score(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print(cm)
 
 
 sample_data = pd.DataFrame([[35, 1, 50000]], columns=["Age", "Gender", "AccountBalance"])
